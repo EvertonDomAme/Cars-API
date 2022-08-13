39144fc8e1977926c8130be6db79bd8246d6cd40
Andressa Miyaguti <oliveirameloeverton@gmail.com>
2022-08-13 13:06:02 -0300

    Updated index.js
    Add a testing base '/' route

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//testing base route
+app.get('/', function (req, res) {
+  res.send('Running');
+});
+
 app.use("/cars", route);
 
 app.listen(port, () => {
