6d07c120d9e7f4573cc01d5e55d976ee4a43b17b
Andressa Miyaguti <oliveirameloeverton@gmail.com>
2022-08-13 12:55:18 -0300

    Add /models/car.js
    Using mongoose for MongoDB
    
    Car Schema defined

---
+++ src/models/car.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const CarSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  color: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  manufacture_year: { type: Number, required: true },
+  model_year: { type: Number, required: true },
+  selling_price: { type: Number, required: true },
+  description: { type: String, required: true },
+});
+
+const Car = mongoose.model('cars', CarSchema);
+
+module.exports = Car;
