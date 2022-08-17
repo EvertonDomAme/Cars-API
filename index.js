dd808d9293467ce31c24d7a952e81d5f833924aa
Andressa Miyaguti <oliveirameloeverton@gmail.com>
2022-08-16 21:04:55 -0300

    Update Index.js
    Modified namings
    Initialized Database Connection
    Defined Routes to access database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const connectToDatabase = require('./src/database/database');
+const route = require('./src/routes/vehicles.route');
 
 const port = 3210;
 const app = express();
@@ -7,12 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+connectToDatabase();
+
 //testing base route
 app.get('/', function (req, res) {
-  res.send('Running');
+  res.send();
 });
 
-app.use("/cars", route);
+app.use("/vehicles", route);
 
 app.listen(port, () => {
   console.log(`Server running on => http://localhost:${port}`);
